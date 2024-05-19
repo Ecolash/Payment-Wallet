c0263d91a2edd872d64c965e30d89213f4dfb081
Diganta Mandal <digantamandal@Digantas-MacBook-Air.local>
2024-05-19 15:53:21 +0530

    One try

--- apps/user-app/components/Sidebaritem.tsx
+++ apps/user-app/components/Sidebaritem.tsx
@@ -7,6 +7,9 @@ export const SidebarItem=({href,title,icon}:{href:string,title:string,icon:React
     const pathname=usePathname();
     const selected=pathname===href;
 
+
+    
+
     return(
         <div className={`flex ${selected? "text-[#6a51a6]" : "text-slate-500"} cursor-pointer p-2 pl-8`} onClick={()=>{
             router.push(href);
