f113e48d647d9a06c180314c3ecc560cd313a740
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-05-28 19:11:34 +0530

    lets check

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,15 +1,20 @@
+"use server"
+
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import { authOptions } from "./lib/auth";
+import Link from "next/link";
 
+export default async function Page() {
+  const session = await getServerSession(authOptions);
 
-
-export  default async function Page(){
-  const session=await getServerSession(authOptions);
-  if(session?.user){
-    redirect('/dashboard');
-  }else{
-    redirect('/api/auth/signin');
-  }
-
+  return (
+    <div>
+      <h1>Page</h1>
+      <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3 my-3" href={
+        session?.user ? "/dashboard" : "/api/auth/signin"
+      }>
+        {session?.user ? "Go to dashboard" : "Sign in"}
+      </Link>
+    </div>
+  );
 }
