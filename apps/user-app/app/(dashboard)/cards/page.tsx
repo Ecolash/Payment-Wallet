b5b7da2423498782d160ca71a76823d8ad0d66ac
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-06-02 09:59:45 +0530

    Fixed typo

--- apps/user-app/app/(dashboard)/cards/page.tsx
+++ apps/user-app/app/(dashboard)/cards/page.tsx
@@ -1,11 +1,9 @@
-import prisma from '@repo/db/client';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../../lib/auth';
-import React from 'react';
-import Dashboard from '../../../components/CardDashboard';
-import { CardsType } from '@prisma/client';
-
-
+import prisma from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import React from "react";
+import Dashboard from "../../../components/CardDashboard";
+import { CardsType } from "@prisma/client";
 
 interface CardDashboard {
   card_number: string;
@@ -15,35 +13,36 @@ interface CardDashboard {
   card_expiry: string;
 }
 
-async function getCardData(){
-  const session=await getServerSession(authOptions);
-  const cards=await prisma.cards.findMany({
-    where:{
-      userId:Number(session?.user?.id)
-    }
+async function getCardData() {
+  const session = await getServerSession(authOptions);
+  const cards = await prisma.cards.findMany({
+    where: {
+      userId: Number(session?.user?.id),
+    },
   });
-  return cards.map((c:CardDashboard)=>({
-    accountNumber:c.card_number,
-    cardHolderName:c.card_name,
-    cvv:c.card_cvc,
-    cardType:c.card_type,
-    expiryDate:c.card_expiry
-  }))
+  return cards.map((c: CardDashboard) => ({
+    accountNumber: c.card_number,
+    cardHolderName: c.card_name,
+    cvv: c.card_cvc,
+    cardType: c.card_type,
+    expiryDate: c.card_expiry,
+  }));
 }
 
-export default async function() {
-  const cardData=await getCardData();
+export default async function () {
+  const cardData = await getCardData();
   return (
     <div>
-      <Dashboard cards={cardData.map(card => ({
-        bankName: "HDFC BANK",//Hard Coded Now
-        accountNumber: card.accountNumber,
-        cardHolderName: card.cardHolderName,
-        cvv: card.cvv,
-        cardType: card.cardType ,
-        expiryDate: card.expiryDate,
-      }))} />
+      <Dashboard
+        cards={cardData.map((card) => ({
+          bankName: "HDFC BANK", //Hard Coded Now
+          accountNumber: card.accountNumber,
+          cardHolderName: card.cardHolderName,
+          cvv: card.cvv,
+          cardType: card.cardType,
+          expiryDate: card.expiryDate,
+        }))}
+      />
     </div>
   );
-};
-
+}
--- apps/user-app/components/AddCard.tsx
+++ apps/user-app/components/AddCard.tsx
@@ -1,47 +1,48 @@
-"use client"
-import React,{useState} from "react";
-import Cards, { Focused } from 'react-credit-cards-2'
-import 'react-credit-cards-2/dist/es/styles-compiled.css';
+"use client";
+import React, { useState } from "react";
+import Cards, { Focused } from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
 import { addNewCard } from "../app/lib/actions/addNewCard";
 
-interface CardInfo{
-    number:string,
-    expiry:string,
-    cvc:string,
-    name:string,
-    focus:Focused
+interface CardInfo {
+  number: string;
+  expiry: string;
+  cvc: string;
+  name: string;
+  focus: Focused;
 }
-export const AddCardForm=()=>{
-    const [state,setState]=useState<CardInfo>({
-        number: '',
-        expiry: '',
-        cvc: '',
-        name: '',
-        focus: '',
-    });
-    const handleInputChange = (evt:any) => {
-        const { name, value } = evt.target;
-        setState((prev) => ({ ...prev, [name]: value }));
-    }
-    
-    const handleInputFocus = (evt:any) => {
-        setState((prev) => ({ ...prev, focus: evt.target.name }));
-    }
-    return <div>
-        <div className="rccs__card rccs__card--unknown">
-        <Cards
-            cvc={state.cvc}
-            expiry={state.expiry}
-            focused={state.focus}
-            name={state.name}
-            number={state.number}
-        />
-        </div>
+export const AddCardForm = () => {
+  const [state, setState] = useState<CardInfo>({
+    number: "",
+    expiry: "",
+    cvc: "",
+    name: "",
+    focus: "",
+  });
+  const handleInputChange = (evt: any) => {
+    const { name, value } = evt.target;
+    setState((prev) => ({ ...prev, [name]: value }));
+  };
 
-        <form>
+  const handleInputFocus = (evt: any) => {
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
+  };
+  return (
+    <div>
+      <div className="rccs__card rccs__card--unknown">
+        <Cards
+          cvc={state.cvc}
+          expiry={state.expiry}
+          focused={state.focus}
+          name={state.name}
+          number={state.number}
+        />
+      </div>
+
+      <form>
         <div className="row ">
           <div className="col-sm-11 text-black">
-            <label >Card Number</label>
+            <label>Card Number</label>
             <input
               type="text"
               className="form-control"
@@ -55,7 +56,7 @@ export const AddCardForm=()=>{
         <br />
         <div className="row">
           <div className="col-sm-11 text-black">
-            <label >Card Name</label>
+            <label>Card Name</label>
             <input
               type="text"
               className="form-control"
@@ -69,7 +70,7 @@ export const AddCardForm=()=>{
         <br />
         <div className="row">
           <div className="col-sm-6 text-black">
-            <label >Expiration Date</label>
+            <label>Expiration Date</label>
             <input
               type="text"
               name="expiry"
@@ -80,7 +81,7 @@ export const AddCardForm=()=>{
             ></input>
           </div>
           <div className="col-sm-5 text-black">
-            <label >CVV</label>
+            <label>CVV</label>
             <input
               type="tel"
               name="cvc"
@@ -92,11 +93,17 @@ export const AddCardForm=()=>{
           </div>
         </div>
         <div className="flex justify-center">
-            <button className="text-black" onClick={async()=>{
-                const json=await addNewCard({cardData:state});
-                window.alert(json.message);
-            }}>Submit</button>
+          <button
+            className="text-black"
+            onClick={async () => {
+              const json = await addNewCard({ cardData: state });
+              window.alert(json.message);
+            }}
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>
-}
+  );
+};
